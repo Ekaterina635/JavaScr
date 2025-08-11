e399fcb665c292511e45348250ca42a0389129c1
Ekaterina635 <katringus21@gmail.com>
2025-08-11 18:05:03 +0200

    Update script.js
    комментарии про результат, если задать переменную ещё раз

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let price=150
-
+/* 
+Если задать переменную ещё раз или задать, как константу, при существующей переменной, 
+то результат схожий, консоль выводит ошибку, выходит сообщение: SyntaxError: Identifier 'price' has already been declared
+*/
 
 console.log(price)
+
